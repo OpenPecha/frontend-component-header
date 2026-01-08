8d743a78d279e864efce829c294cee979e6c73d7
tenkalden <tenkalden007@gmail.com>
2026-01-08 12:40:08 +0530

    added dist

--- dist/learning-header/LearningHeaderCourseInfo.js
+++ dist/learning-header/LearningHeaderCourseInfo.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+const renderMixedText = text => {
+  if (!text) return null;
+  const tibetanRegex = /[\u0F00-\u0FFF]+/g;
+  const parts = [];
+  let lastIndex = 0;
+  let match;
+  while ((match = tibetanRegex.exec(text)) !== null) {
+    if (match.index > lastIndex) {
+      parts.push(text.substring(lastIndex, match.index));
+    }
+    parts.push(/*#__PURE__*/React.createElement("span", {
+      key: match.index,
+      style: {
+        fontFamily: 'Jomolhari, serif',
+        fontSize: '1.15em'
+      }
+    }, match[0]));
+    lastIndex = match.index + match[0].length;
+  }
+  if (lastIndex < text.length) {
+    parts.push(text.substring(lastIndex));
+  }
+  return parts.length > 0 ? parts : text;
+};
 const LearningHeaderCourseInfo = _ref => {
   let {
     courseOrg,
@@ -11,10 +35,8 @@ const LearningHeaderCourseInfo = _ref => {
       minWidth: 0
     }
   }, /*#__PURE__*/React.createElement("span", {
-    className: "d-block small m-0"
-  }, courseOrg, " ", courseNumber), /*#__PURE__*/React.createElement("span", {
     className: "d-block m-0 font-weight-bold course-title"
-  }, courseTitle));
+  }, renderMixedText(courseTitle)));
 };
 export const courseInfoDataShape = {
   courseOrg: PropTypes.string,
